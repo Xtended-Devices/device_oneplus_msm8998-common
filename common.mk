e7af2f5c9be3252bd2f7486aba71b7943e58331a
sathish2026 <txt2sathish@gmail.com>
2018-03-01 14:27:45 +0530

    msm8998: Extend face detection

--- common.mk
+++ common.mk
@@ -206,6 +206,11 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.0-service.widevine
 
+# Face detection
+PRODUCT_PACKAGES += \
+    org.codeaurora.camera \
+    org.codeaurora.camera.xml
+
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service \
