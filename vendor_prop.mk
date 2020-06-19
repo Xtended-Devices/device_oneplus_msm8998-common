47788555b553cbfbab01c56fa313698850eaea6e
ctwoon <ctwoon@pixelexperience.org>
2020-06-19 11:37:11 +0530

    msm8998-common: props: Enable zygote preforking
    faster app launching, as referenced in https://forum.xda-developers.com/asus-zenfone-max-pro-m1/how-to/android-10-faster-app-launch-enabling-t4108817

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -390,3 +390,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wi-Fi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
+
+# Zygote preforking
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.device_config.runtime_native.usap_pool_enabled=true
