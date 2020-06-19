42a13d4eb56defaced46133bd9725e6319b57df0
mukesh22584 <mks22584@gmail.com>
2020-06-19 20:41:49 +0530

    msm8998-common: add missing CryptfsHW

--- common.mk
+++ common.mk
@@ -172,6 +172,10 @@ PRODUCT_PACKAGES += \
     com.quicinc.cne \
     services-ext
 
+# CryptfsHW
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
