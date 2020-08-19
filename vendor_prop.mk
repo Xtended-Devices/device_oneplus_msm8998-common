de3622855efebeca6103ca40bbd9151047a3a02f
mukesh22584 <mks22584@gmail.com>
2020-08-20 01:44:00 +0530

    msm8998-common: props: Remove zygote preforking
    This reverts commit 47788555b553cbfbab01c56fa313698850eaea6e.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -401,7 +401,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wi-Fi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
-
-# Zygote preforking
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.device_config.runtime_native.usap_pool_enabled=true
