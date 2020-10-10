879c4b8e782237ab93d26edbd541c026e71f7acf
Adhitya Mohan <me@adhityamohan.in>
2020-10-10 09:46:32 +0530

    msm8998-common: Completely rewrite our init script * Use some newer functions, the older really don't work at all * Load dalvik heap dynamically based on ram variants * Get rid of useless functions (newer alarm blobs no longer need this) * Override with coral october fp

--- init/init_oneplus_msm8998.cpp
+++ init/init_oneplus_msm8998.cpp
@@ -1,8 +1,5 @@
 /*
-   Copyright (C) 2007, The Android Open Source Project
-   Copyright (c) 2016, The CyanogenMod Project
-   Copyright (c) 2017, The LineageOS Project
-
+   Copyright (c) 2014, The Linux Foundation. All rights reserved.
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -15,7 +12,6 @@
     * Neither the name of The Linux Foundation nor the names of its
       contributors may be used to endorse or promote products derived
       from this software without specific prior written permission.
-
    THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
@@ -29,45 +25,64 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <stdlib.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+#include <sys/sysinfo.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
 #include <android-base/file.h>
 #include <android-base/logging.h>
-#include <android-base/properties.h>
 #include <android-base/strings.h>
-
-#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/_system_properties.h>
-
-#include "vendor_init.h"
 
 #define DEVINFO_FILE "/sys/project_info/project_name"
 #define SENSOR_VERSION_FILE "/sys/devices/soc/soc:fingerprint_detect/sensor_version"
 
+#include <android-base/properties.h>
+#include "vendor_init.h"
+
 using android::base::Trim;
 using android::base::GetProperty;
 using android::base::ReadFileToString;
-using android::base::SetProperty;
 
-void property_override(const std::string& name, const std::string& value)
+void property_override(char const prop[], char const value[])
 {
-    size_t valuelen = value.size();
+    prop_info *pi;
 
-    prop_info* pi = (prop_info*) __system_property_find(name.c_str());
-    if (pi != nullptr) {
-        __system_property_update(pi, value.c_str(), valuelen);
-    }
-    else {
-        int rc = __system_property_add(name.c_str(), name.size(), value.c_str(), valuelen);
-        if (rc < 0) {
-            LOG(ERROR) << "SetProperty(\"" << name << "\", \"" << value << "\") failed: "
-                       << "__system_property_add failed";
-        }
+    pi = (prop_info*) __system_property_find(prop);
+    if (pi)
+        __system_property_update(pi, value, strlen(value));
+    else
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void property_override_multi(char const system_prop[], char const vendor_prop[],char const bootimage_prop[],
+    char const value[])
+{
+    property_override(system_prop, value);
+    property_override(vendor_prop, value);
+    property_override(bootimage_prop, value);
+}
+
+void load_dalvikvm_properties()
+{
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+    if (sys.totalram < 7000ull * 1024 * 1024) {
+        //6GB RAM
+        property_override("dalvik.vm.heapstartsize", "16m");
+        property_override("dalvik.vm.heaptargetutilization", "0.5");
+        property_override("dalvik.vm.heapmaxfree", "32m");
+    } else {
+        //8GB RAM
+        property_override("dalvik.vm.heapstartsize", "24m");
+        property_override("dalvik.vm.heaptargetutilization", "0.46");
+        property_override("dalvik.vm.heapmaxfree", "48m");
     }
 }
 
@@ -81,23 +96,23 @@ void init_target_properties()
 
         if (!strncmp(device.c_str(), "16859", 5)) {
             // Oneplus 5
-            SetProperty("ro.display.series", "OnePlus 5");
+            property_override("ro.display.series", "OnePlus 5");
             unknownDevice = false;
         }
         else if (!strncmp(device.c_str(), "17801", 5)) {
             // Oneplus 5T
-            SetProperty("ro.display.series", "OnePlus 5T");
+            property_override("ro.display.series", "OnePlus 5T");
             unknownDevice = false;
         }
 
-        SetProperty("vendor.boot.project_name", device.c_str());
+        property_override("vendor.boot.project_name", device.c_str());
     }
     else {
         LOG(ERROR) << "Unable to read device info from " << DEVINFO_FILE;
     }
 
     if (unknownDevice) {
-        SetProperty("ro.display.series", "UNKNOWN");
+        property_override("ro.display.series", "UNKNOWN");
     }
 }
 
@@ -108,10 +123,10 @@ void init_fingerprint_properties()
     if (ReadFileToString(SENSOR_VERSION_FILE, &sensor_version)) {
         LOG(INFO) << "Loading Fingerprint HAL for sensor version " << sensor_version;
         if (Trim(sensor_version) == "1" || Trim(sensor_version) == "2") {
-            SetProperty("ro.hardware.fingerprint", "fpc");
+            property_override("ro.hardware.fingerprint", "fpc");
         }
         else if (Trim(sensor_version) == "3") {
-            SetProperty("ro.hardware.fingerprint", "goodix");
+            property_override("ro.hardware.fingerprint", "goodix");
         }
         else {
             LOG(ERROR) << "Unsupported fingerprint sensor: " << sensor_version;
@@ -122,73 +137,13 @@ void init_fingerprint_properties()
     }
 }
 
-void init_alarm_boot_properties()
+
+void vendor_load_properties()
 {
-    char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
-    std::string boot_reason;
-
-    if (ReadFileToString(boot_reason_file, &boot_reason)) {
-        /*
-         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
-         * For existing PMIC chips, the following mapping applies
-         * for the value of boot_reason:
-         *
-         * 0 -> unknown
-         * 1 -> hard reset
-         * 2 -> sudden momentary power loss (SMPL)
-         * 3 -> real time clock (RTC)
-         * 4 -> DC charger inserted
-         * 5 -> USB charger inserted
-         * 6 -> PON1 pin toggled (for secondary PMICs)
-         * 7 -> CBLPWR_N pin toggled (for external power supply)
-         * 8 -> KPDPWR_N pin toggled (power key pressed)
-         */
-        if (Trim(boot_reason) == "0") {
-            SetProperty("ro.boot.bootreason", "invalid");
-            SetProperty("ro.alarm_boot", "false");
-        }
-        else if (Trim(boot_reason) == "1") {
-            SetProperty("ro.boot.bootreason", "hard_reset");
-            SetProperty("ro.alarm_boot", "false");
-        }
-        else if (Trim(boot_reason) == "2") {
-            SetProperty("ro.boot.bootreason", "smpl");
-            SetProperty("ro.alarm_boot", "false");
-        }
-        else if (Trim(boot_reason) == "3") {
-            SetProperty("ro.alarm_boot", "true");
-            // disable boot animation for RTC wakeup
-            SetProperty("debug.sf.nobootanimation", "1");
-        }
-        else if (Trim(boot_reason) == "4") {
-            SetProperty("ro.boot.bootreason", "dc_chg");
-            SetProperty("ro.alarm_boot", "false");
-        }
-        else if (Trim(boot_reason) == "5") {
-            SetProperty("ro.boot.bootreason", "usb_chg");
-            SetProperty("ro.alarm_boot", "false");
-        }
-        else if (Trim(boot_reason) == "6") {
-            SetProperty("ro.boot.bootreason", "pon1");
-            SetProperty("ro.alarm_boot", "false");
-        }
-        else if (Trim(boot_reason) == "7") {
-            SetProperty("ro.boot.bootreason", "cblpwr");
-            SetProperty("ro.alarm_boot", "false");
-        }
-        else if (Trim(boot_reason) == "8") {
-            SetProperty("ro.boot.bootreason", "kpdpwr");
-            SetProperty("ro.alarm_boot", "false");
-        }
-    }
-    else {
-        LOG(ERROR) << "Unable to read bootreason from " << boot_reason_file;
-    }
-}
-
-void vendor_load_properties() {
-    LOG(INFO) << "Loading vendor specific properties";
+    // fingerprint
+    property_override("ro.build.description", "coral-user 11 RP1A.201005.004 6782484 release-keys");
+    property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys");
     init_target_properties();
     init_fingerprint_properties();
-    init_alarm_boot_properties();
+    load_dalvikvm_properties();
 }
