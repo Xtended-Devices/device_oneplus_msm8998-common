50c39ff4ced39c5d9a570b837fb4d0ea15244e6b
Pranav Vashi <neobuddy89@gmail.com>
2021-01-15 08:50:18 +0530

    msm8998-common: Update WFD from LA.UM.9.6.2.r1-03300-89xx.0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I657cfa5d2e0ac6102fe6e3b3d00e4e09dbcfb6a9

--- common.mk
+++ common.mk
@@ -208,6 +208,8 @@ PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
     libhwc2on1adapter \
     libqdMetaData \
+    libqdMetaData.system \
+    libqdMetaData.vendor \
     libtinyxml \
     libvulkan
 
@@ -535,6 +537,10 @@ PRODUCT_COPY_FILES += \
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
     libaacwrapper \
+    libavservices_minijail \
+    libavservices_minijail.vendor \
+    libmediaextractorservice \
+    libminijail \
     libnl
 
 PRODUCT_BOOT_JARS += \
