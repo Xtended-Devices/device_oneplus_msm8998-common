e5b82898ccbfed2170ffd4410f9d0dbbe58c2b4e
mukesh22584 <mks22584@gmail.com>
2021-02-05 17:37:34 +0530

    msm8998-common: update coral FP to Feb

--- init/init_oneplus_msm8998.cpp
+++ init/init_oneplus_msm8998.cpp
@@ -147,7 +147,7 @@ void init_fingerprint_properties()
 void vendor_load_properties()
 {
     // fingerprint
-    property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/coral/coral:11/RQ1A.210105.003/7005429:user/release-keys");
+    property_override_multi("ro.build.fingerprint", "ro.vendor.build.fingerprint","ro.bootimage.build.fingerprint", "google/coral/coral:11/RQ1A.210205.004/7038034:user/release-keys");
     init_target_properties();
     init_fingerprint_properties();
     load_dalvikvm_properties();
