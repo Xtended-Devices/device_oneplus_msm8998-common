4f8a3f2bf4a32ba01d8f34e91698ab2677231578
Sonal Singh <sonal.singh.19993@gmail.com>
2021-05-02 21:36:25 +0530

    msm8998-common: prebuilts: Add GCamGOPrebuilt

--- common.mk
+++ common.mk
@@ -179,7 +179,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    Snap
+    GCamGOPrebuilt
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0
