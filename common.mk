db05d099aaf38e921cd3d1821ca49f2046e81f54
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-15 11:48:48 +0800

    msm8998-common: Build all extra VNDK versions * Basically make system a GSI, but this should avoid us the headache   made by VNDK libs not being built based on proprietary interfaces
    Change-Id: I1ca876b5d2a82d41af76f4c327b89a181bb3d6bd

--- common.mk
+++ common.mk
@@ -443,6 +443,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
 
 # VNDK
+PRODUCT_EXTRA_VNDK_VERSIONS := 29 30
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libcutils-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libcutils-v29.so
